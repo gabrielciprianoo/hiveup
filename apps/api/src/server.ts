badf0bcf2c51a4f367ebf3cf0918d4a12e0dbfbc
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-09-04 19:58:40 -0600

    ✨ Set up Express application with JSON parsing and project routes

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import { conectDb } from "./config/db";
+import projectRoutes from './routes/projectRoutes';
 
 dotenv.config();
 
 conectDb();
 
+
 const app: express.Application = express();
+
+app.use(express.json());
+app.use('/api/projects',  projectRoutes);
 
 export default app;
