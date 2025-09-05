c028c7ad7e109d255c77760f312f7a1b5efee645
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-09-04 19:58:48 -0600

    ✨ Implement ProjectController with methods to get and create projects

---
+++ apps/api/src/controllers/ProjectController.ts
@@ -0,0 +1,22 @@
+import type { Request, Response } from "express";
+import Project from "../models/Project";
+
+
+export class ProjectController { 
+
+    static getAllProjects = async ( request : Request, response : Response)=> {
+        response.send('all projects');
+    }
+    
+    static createProject = async ( request : Request, response : Response)=> {
+        const project = new Project(request.body);
+
+        try {
+            await project.save()
+            response.send('Proyecto Creado Correctamente')
+        } catch (error) {
+            console.log(error)
+        }
+        response.send('create project');
+    }
+}
