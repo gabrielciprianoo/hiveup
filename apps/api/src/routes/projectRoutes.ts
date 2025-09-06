6c50f1ea2d7884b2135af1e80df4997112bea1cd
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-09-05 18:06:58 -0600

    ✨ Refactor project creation route to use centralized validation rules

--- apps/api/src/routes/projectRoutes.ts
+++ apps/api/src/routes/projectRoutes.ts
@@ -1,15 +1,13 @@
 import { Router } from "express";
 import { ProjectController } from "../controllers/ProjectController";
-import { body } from "express-validator";
 import { handleInputErrors } from "../middleware/validation";
+import { createProjectRules } from "../validators/project.validators";
 
  const router: Router = Router();
 
  router.get('/', ProjectController.getAllProjects);
  router.post('/', 
-    body('projectName').notEmpty().withMessage('El nombre del proyecto es obligatorio'),
-    body('clientName').notEmpty().withMessage('El nombre del cliente es obligatorio'),
-    body('description').notEmpty().withMessage('La descripción es obligatorio'),
+    createProjectRules,
     handleInputErrors,
     ProjectController.createProject
 );
