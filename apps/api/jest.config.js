89caa121b1595700085b5c3f2fcb735d894b24ea
gabrielciprianoo <devgabrielcipriano@gmail.com>
2026-02-17 09:49:48 -0600

    ⚙️ Add test config and update gitignore

---
+++ apps/api/jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  collectCoverageFrom: ['src/**/*.ts', '!src/index.ts'],
+  coverageDirectory: 'coverage',
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  testTimeout: 60000,
+  forceExit: true,
+  detectOpenHandles: false,
+};
