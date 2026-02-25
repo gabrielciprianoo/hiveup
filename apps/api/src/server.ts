92f6eab1d5b57d76b7dea0ca5a74044d3772b0d1
gabrielciprianoo <devgabrielcipriano@gmail.com>
2026-02-25 11:33:55 -0600

    🧱 config: add CORS configuration

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,18 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import { conectDb } from "./config/db";
-import projectRoutes from './routes/projectRoutes';
-import taskToutes from './routes/taskRoutes';
+import projectRoutes from "./routes/projectRoutes";
+import taskToutes from "./routes/taskRoutes";
+import cors from "cors";
+import { corsConfig } from "./config/cors";
 
 dotenv.config();
 
 conectDb();
 
-
 const app: express.Application = express();
 
+app.use(cors(corsConfig));
+
 app.use(express.json());
-app.use('/api/projects',  projectRoutes);
-app.use('/api/projects/:projectId/tasks',  taskToutes);
+app.use("/api/projects", projectRoutes);
+app.use("/api/projects/:projectId/tasks", taskToutes);
 
 export default app;
