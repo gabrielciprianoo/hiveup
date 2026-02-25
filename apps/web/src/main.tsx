581f236eddf3342e235d80cca8f23418f0af1fbe
gabrielciprianoo <devgabrielcipriano@gmail.com>
2026-02-25 12:31:48 -0600

    ✨ feat: add @tanstack/react-query dependency and integrate QueryClientProvider in main.tsx

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,27 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Router from './router'
-import { UserProvider } from './contexts'
-import { Toaster } from "sileo"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Router from "./router";
+import { UserProvider } from "./contexts";
+import { Toaster } from "sileo";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <UserProvider>
-      <Toaster 
-        position="top-right"
-        offset={{ top: 60, right: 16 }}
-        options={{
-          fill: "#FFFFFF",
-          roundness: 12,
-          styles: {
-            title: "text-dark font-medium",
-            description: "text-dark/70",
-            badge: "bg-primary/20",
-          }
-        }}
-      />
-      <Router/>
-    </UserProvider>
+    <QueryClientProvider client={queryClient}>
+      <UserProvider>
+        <Toaster
+          position="top-right"
+          offset={{ top: 60, right: 16 }}
+          options={{
+            fill: "#FFFFFF",
+            roundness: 12,
+            styles: {
+              title: "text-dark font-medium",
+              description: "text-dark/70",
+              badge: "bg-primary/20",
+            },
+          }}
+        />
+        <Router />
+      </UserProvider>
+    </QueryClientProvider>
   </StrictMode>,
-)
+);
