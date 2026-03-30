9734e03ef9e2048e3196f9ad0dfb77e10c35e361
gabrielciprianoo <devgabrielcipriano@gmail.com>
2026-03-30 16:40:45 -0600

    📅 feat: create formatDate utility function

---
+++ apps/web/src/utils/formatDate.ts
@@ -0,0 +1,11 @@
+const formatter = new Intl.DateTimeFormat("es-ES", {
+  day: "2-digit",
+  month: "short",
+  year: "numeric",
+  hour: "2-digit",
+  minute: "2-digit",
+});
+
+export function formatDate(dateString: string): string {
+  return formatter.format(new Date(dateString));
+}
