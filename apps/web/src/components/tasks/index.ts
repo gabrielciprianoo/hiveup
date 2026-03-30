c6e75c40c91fd9964e05335b57a64664c137fe4a
gabrielciprianoo <devgabrielcipriano@gmail.com>
2026-03-30 16:00:15 -0600

    📦 refactor: export EditTaskModal and EditTaskQuery

--- apps/web/src/components/tasks/index.ts
+++ apps/web/src/components/tasks/index.ts
@@ -1,2 +1,4 @@
 export { default as AddTaskModal } from "./AddTaskModal";
 export { default as TasksList } from "./TasksList";
+export { default as EditTaskQuery } from "./EditTaskQuery";
+export { default as EditTaskModal } from "./EditTaskModal";
