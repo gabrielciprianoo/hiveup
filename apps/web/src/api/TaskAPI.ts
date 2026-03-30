c73c190b35b73cf5e9ea4530f83f642f3c917f98
gabrielciprianoo <devgabrielcipriano@gmail.com>
2026-03-30 16:00:03 -0600

    🔌 feat: add getTaskById and updateTask API functions

--- apps/web/src/api/TaskAPI.ts
+++ apps/web/src/api/TaskAPI.ts
@@ -2,12 +2,16 @@ import { isAxiosError } from "axios";
 import api from "../lib/axios";
 import { type Task, type TaskFormData } from "../types";
 
-export type TaskAPI = {
+export type TaskAPIParams = {
   projectId: Task["_id"];
+  taskId: Task["_id"];
   formData: TaskFormData;
 };
 
-export async function createTask({ projectId, formData }: TaskAPI) {
+export async function createTask({
+  projectId,
+  formData,
+}: Pick<TaskAPIParams, "projectId" | "formData">) {
   try {
     const { data } = await api.post<Task>(
       `/projects/${projectId}/tasks`,
@@ -24,6 +28,40 @@ export async function createTask({ projectId, formData }: TaskAPI) {
 export async function getTasks(projectId: string) {
   try {
     const { data } = await api.get(`/projects/${projectId}/tasks`);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+export async function getTaskById({
+  projectId,
+  taskId,
+}: Pick<TaskAPIParams, "projectId" | "taskId">) {
+  try {
+    const { data } = await api.get<Task>(
+      `/projects/${projectId}/tasks/${taskId}`,
+    );
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+export async function updateTask({
+  projectId,
+  taskId,
+  formData,
+}: Pick<TaskAPIParams, "projectId" | "taskId" | "formData">) {
+  try {
+    const { data } = await api.put<string>(
+      `/projects/${projectId}/tasks/${taskId}`,
+      formData,
+    );
     return data;
   } catch (error) {
     if (isAxiosError(error) && error.response) {
